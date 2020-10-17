d243c048b3070fb934ce5c881a0c2114bd81cddc
Fernando <fernando.85@outlook.com>
2020-10-17 02:18:33 -0300

    react use form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,30 @@
-import React from "react";
-import Navbar from "./Navbar";
-import TodoList from "./TodoList";
+
+import React from 'react';
+import Navbar from './Navbar';
+import TodoList from './TodoList';
+import TodoContext from '../contexts/TodoContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AddTodo from './AddTodo';
 
 const App = () => {
     return(
-        <div className="uk-container">
-            <Navbar></Navbar>
-            <TodoList></TodoList>
-        </div>
+        <TodoContext>
+            <Router>
+                <Navbar></Navbar>
+                <br />
+                <div className="uk-container">
+                    <Switch>
+                        <Route path="/create">
+                            <AddTodo></AddTodo>
+                        </Route>
+                        <Route path="/">
+                            <h4>Minha lista de tarefas</h4>
+                            <TodoList></TodoList>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </TodoContext>
     )
 }
 
